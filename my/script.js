044c1f644e14c72737ad571ce484ed9156e3ff14
naeem7w <hckre9633@gmail.com>
2025-03-06 02:20:28 +0500

    Update script.js

--- my/script.js
+++ my/script.js
@@ -1,3 +1,4 @@
+console.log("script.js is loaded!");
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
     alert("Login successful! Redirecting...");
